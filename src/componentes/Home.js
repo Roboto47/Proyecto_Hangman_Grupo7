7348b9af33d437fafb2ee0a7420c6c5f303a4299
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-10-07 17:17:38 -0300

    Actualización
    Modificado Home.

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -6,67 +6,49 @@ import jugar from '../asset/imagenes/jugar.png';
 import mate from '../asset/imagenes/mate.png';
 import Boton from './BotonHome.js';
 import loguito from '../asset/imagenes/loguito.png';
-class Home extends React.Component{
 
 
-render(){
-
-
-return(
+export default function Home (){
 
 
 
-<div>
 
-  <section className="icono">
+  return ( <div>
+
+<section className="icono">
 <img  src={mate} alt="" width="70" />
-  <img  src={loguito} alt="" width="300" />
+<img  src={loguito} alt="" width="300" />
+</section>
+      <div >
+          <div>
+          <section className="logohome">
+  <img  src={logo} alt="" width="500" height={190}/>
+  <img  src={sogaLogo} alt="" width="45" /> 
   </section>
+          <section className="botones">
+              <Boton/>
+          </section>
+          </div>
+          
+      </div>
 
-  <section className="logohome">
-    <img  src={logo} alt="" width="500" height={190}/>
-    <img  src={soga} alt="" width="45" />
+      <section className="derechos">
 
-    <div className='contenedorbotonhomes'>
-            <a href="/">  <button class="botoneshome " >  <span>JUGAR</span>   </button>  </a>   
-            <a href="/desarrolladores">  <button class="botoneshome " >  <span>Equipo</span>   </button>  </a>   
-            </div>
-    </section>
-  
-    <section className="derechoshome">
-           <h4> © 2022 Mate Cebado Inc. | All rights reserved.  </h4>
-           <img  src={mate} width={43}/>
-        </section>
-    
+      <h4> © 2022 Mate Cebado Games Inc. | All rights reserved. </h4>
+      <img  src={mate} alt="error" width={43}/>
+      </section>
 
-  
-  </div>
+      <div className="redes2">
+      <img  src={twitter} alt="" width={60} height={50}/>
+      <img  src={face} alt="" width={60} height={50}/>
+      <img  src={discord} alt="" width={62} height={50} />
 
-
-
+    </div>
+     
 
 
- 
+</div>
 
-
-
-
-)
-
-
-
-
-
-
-
-
-
+      
+  )
 }
-
-
-
-
-
-}
-
-export default Home;
