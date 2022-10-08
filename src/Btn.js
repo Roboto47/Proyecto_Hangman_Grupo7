a931d1c9b74fe8b9c344dbfc575b883e02fdd61b
Valeria Emilia Caballero <112204865+ValeriaEmilia000@users.noreply.github.com>
2022-10-07 23:39:46 -0300

    correcciones

--- src/Btn.js
+++ src/Btn.js
@@ -10,6 +10,7 @@ export default function Btn(){
     
    
     <CustomLink to="/juego" className="botonHome" >JUGAR</CustomLink>
+    <br></br>
     <CustomLink to="/desarrolladores" className="botonHome">EQUIPO </CustomLink>
     
 
